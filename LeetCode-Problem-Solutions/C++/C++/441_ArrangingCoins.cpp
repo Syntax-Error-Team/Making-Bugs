f25f9c11a41fc7fc5eeea6bf76d16aa9a5e61433
Shubham-Yadav <shubhamyadav7063@gmail.com>
2022-10-09 23:32:48 +0530

    questions added

---
+++ LeetCode-Problem-Solutions/C++/C++/441_ArrangingCoins.cpp
@@ -0,0 +1,26 @@
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size() - 1;
+        int s = 0, e = n;
+
+        while (s <= e)
+        {
+            int m = s + (e - s) / 2;
+
+            if (nums[m] == target)
+                return m;
+
+            else if (nums[m] > target)
+                e = m - 1;
+
+            else
+                s = m + 1;
+        }
+
+        return -1;
+    }
+};
